41945ab9e905a939b8f97190dfa35e99c6f8ff25
Suraj Rajendra Pardeshi <152958796+SurajPardeshi636@users.noreply.github.com>
2023-12-11 14:52:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Marks = 35;
+
+            if (Marks < 36)
+            {
+                Console.WriteLine("CONGRATULATION Your PASSED ");
+            }
+            else
+            {
+                Console.WriteLine("SORRY Your Failed.....");
+            }
+
+       
+            if (Marks < 30)
+            {
+                Console.WriteLine("it is passed");
+            }
+            else
+            {
+                Console.WriteLine("Failed.....");
+            }
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
